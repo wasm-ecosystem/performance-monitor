99d7d57fd804aa9e0f727d78c3506332711cb9c2
Congcong Cai <congcongcai0907@163.com>
2025-10-13 23:27:22 +0800

    fix pages

--- render/App.tsx
+++ render/App.tsx
@@ -6,9 +6,13 @@ import { fetchData, BenchResult, toLineChartProps } from "./render";
 const App = () => {
   const [enableDarwinArm64, setEnableDarwinArm64] = React.useState(true);
   const [enableLinuxX64, setEnableLinuxX64] = React.useState(true);
+  const [enableLinuxArm64, setEnableLinuxArm64] = React.useState(true);
 
   const [enableO3, setEnableO3] = React.useState(false);
   const [enableOz, setEnableOz] = React.useState(true);
+
+  const [enablePerformance, setEnablePerformance] = React.useState(true);
+  const [enableSize, setEnableSize] = React.useState(false);
 
   const [data, setData] = React.useState<Record<string, BenchResult[]>>({});
 
@@ -32,6 +36,13 @@ const App = () => {
             onChange={(event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => setEnableLinuxX64(checked)}
           />
           <span>linux-x64</span>
+        </Grid>{" "}
+        <Grid>
+          <Checkbox
+            checked={enableLinuxArm64}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => setEnableLinuxArm64(checked)}
+          />
+          <span>linux-arm64</span>
         </Grid>
       </Grid>
       <Grid container spacing={2}>
@@ -50,8 +61,34 @@ const App = () => {
           <span>Oz</span>
         </Grid>
       </Grid>
-
-      <LineChart {...toLineChartProps(data, { enableDarwinArm64, enableLinuxX64, enableO3, enableOz })} height={300} />
+      <Grid container spacing={2}>
+        <Grid>
+          <Checkbox
+            checked={enablePerformance}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => setEnablePerformance(checked)}
+          />
+          <span>performance</span>
+        </Grid>
+        <Grid>
+          <Checkbox
+            checked={enableSize}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => setEnableSize(checked)}
+          />
+          <span>jit size</span>
+        </Grid>
+      </Grid>
+      <LineChart
+        {...toLineChartProps(data, {
+          enableDarwinArm64,
+          enableLinuxX64,
+          enableLinuxArm64,
+          enableO3,
+          enableOz,
+          enablePerformance,
+          enableSize,
+        })}
+        height={300}
+      />
     </div>
   );
 };
--- render/render.ts
+++ render/render.ts
@@ -29,7 +29,7 @@ export async function fetchData(setData: React.Dispatch<Record<string, BenchResu
   setData(data);
 }
 
-const platforms = ["darwin-arm64", "linux-x64"];
+const platforms = ["darwin-arm64", "linux-x64", "linux-arm64"];
 const caseNames = ["parser"];
 
 function getLineKey(platform: string, caseName: string, mode: "O3" | "Oz", metric: "time" | "size") {
@@ -41,38 +41,66 @@ export function toLineChartProps(
   {
     enableDarwinArm64,
     enableLinuxX64,
+    enableLinuxArm64,
     enableO3,
     enableOz,
-  }: { enableDarwinArm64: boolean; enableLinuxX64: boolean; enableO3: boolean; enableOz: boolean }
+    enablePerformance,
+    enableSize,
+  }: {
+    enableDarwinArm64: boolean;
+    enableLinuxX64: boolean;
+    enableLinuxArm64: boolean;
+    enableO3: boolean;
+    enableOz: boolean;
+    enablePerformance: boolean;
+    enableSize: boolean;
+  }
 ): LineChartProps {
+  const isPlatformDisabled = (platform: string) => {
+    return (
+      (platform === "darwin-arm64" && !enableDarwinArm64) ||
+      (platform === "linux-x64" && !enableLinuxX64) ||
+      (platform === "linux-arm64" && !enableLinuxArm64)
+    );
+  };
+
+  if (!enableDarwinArm64 && !enableLinuxX64 && !enableLinuxArm64) return { xAxis: [], series: [] };
   if (!enableO3 && !enableOz) return { xAxis: [], series: [] };
+  if (!enablePerformance && !enableSize) return { xAxis: [], series: [] };
 
   const seriesMap: Record<string, { data: (number | null)[]; label?: string }> = {
     baseline: { data: [], label: "baseline" },
   };
   for (const platform of platforms) {
+    if (isPlatformDisabled(platform)) continue;
     for (const caseName of caseNames) {
-      if (platform === "darwin-arm64" && !enableDarwinArm64) continue;
-      if (platform === "linux-x64" && !enableLinuxX64) continue;
       if (enableO3) {
-        seriesMap[getLineKey(platform, caseName, "O3", "time")] = {
-          data: [],
-          label: `${platform} ${caseName} O3 time`,
-        };
-        seriesMap[getLineKey(platform, caseName, "O3", "size")] = {
-          data: [],
-          label: `${platform} ${caseName} O3 size`,
-        };
+        if (enablePerformance) {
+          seriesMap[getLineKey(platform, caseName, "O3", "time")] = {
+            data: [],
+            label: `${platform} ${caseName} O3 time`,
+          };
+        }
+        if (enableSize) {
+          seriesMap[getLineKey(platform, caseName, "O3", "size")] = {
+            data: [],
+            label: `${platform} ${caseName} O3 size`,
+          };
+        }
       }
       if (enableOz) {
-        seriesMap[getLineKey(platform, caseName, "Oz", "time")] = {
-          data: [],
-          label: `${platform} ${caseName} Oz time`,
-        };
-        seriesMap[getLineKey(platform, caseName, "Oz", "size")] = {
-          data: [],
-          label: `${platform} ${caseName} Oz size`,
-        };
+        if (enablePerformance) {
+          seriesMap[getLineKey(platform, caseName, "Oz", "time")] = {
+            data: [],
+            label: `${platform} ${caseName} Oz time`,
+          };
+        }
+        if (enableSize) {
+          seriesMap[getLineKey(platform, caseName, "Oz", "size")] = {
+            data: [],
+            label: `${platform} ${caseName} Oz size`,
+          };
+        }
       }
     }
   }
@@ -95,20 +123,23 @@ export function toLineChartProps(
 
   for (const fileName in data) {
     const platform = fileName.split("/")[0];
-    if (platform === "darwin-arm64" && !enableDarwinArm64) continue;
-    if (platform === "linux-x64" && !enableLinuxX64) continue;
+    if (isPlatformDisabled(platform)) continue;
     const date = fileName.split("/")[1].split("-")[0];
 
     const caseName = data[fileName][0].name;
     const index = dateIndexMap[date];
 
     if (enableO3) {
-      seriesMap[getLineKey(platform, caseName, "O3", "time")].data[index] = data[fileName][0].results.active.O3.time;
-      seriesMap[getLineKey(platform, caseName, "O3", "size")].data[index] = data[fileName][0].results.active.O3.size;
+      if (enablePerformance)
+        seriesMap[getLineKey(platform, caseName, "O3", "time")].data[index] = data[fileName][0].results.active.O3.time;
+      if (enableSize)
+        seriesMap[getLineKey(platform, caseName, "O3", "size")].data[index] = data[fileName][0].results.active.O3.size;
     }
     if (enableOz) {
-      seriesMap[getLineKey(platform, caseName, "Oz", "time")].data[index] = data[fileName][0].results.active.Oz.time;
-      seriesMap[getLineKey(platform, caseName, "Oz", "size")].data[index] = data[fileName][0].results.active.Oz.size;
+      if (enablePerformance)
+        seriesMap[getLineKey(platform, caseName, "Oz", "time")].data[index] = data[fileName][0].results.active.Oz.time;
+      if (enableSize)
+        seriesMap[getLineKey(platform, caseName, "Oz", "size")].data[index] = data[fileName][0].results.active.Oz.size;
     }
   }
   return {
